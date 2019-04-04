2508757290294d4809fbea9498f32ab2a9e96bc8
mustafadalga <25087769+mustafadalga@users.noreply.github.com>
2019-04-04 19:49:13 +0300

    Add files via upload

---
+++ Puzzle/Form1.Designer.cs
@@ -0,0 +1,289 @@
+﻿namespace Puzzle
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.label1 = new System.Windows.Forms.Label();
+            this.lblgecensure = new System.Windows.Forms.Label();
+            this.lblhamlesayisi = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.tmrsure = new System.Windows.Forms.Timer(this.components);
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.button7 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.button9 = new System.Windows.Forms.Button();
+            this.button8 = new System.Windows.Forms.Button();
+            this.btnkaristir = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label1.ForeColor = System.Drawing.Color.White;
+            this.label1.Location = new System.Drawing.Point(170, 17);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(277, 20);
+            this.label1.TabIndex = 10;
+            this.label1.Text = "Geçen Süre";
+            this.label1.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblgecensure
+            // 
+            this.lblgecensure.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblgecensure.ForeColor = System.Drawing.Color.White;
+            this.lblgecensure.Location = new System.Drawing.Point(171, 40);
+            this.lblgecensure.Name = "lblgecensure";
+            this.lblgecensure.Size = new System.Drawing.Size(276, 20);
+            this.lblgecensure.TabIndex = 11;
+            this.lblgecensure.Text = "00:00:00";
+            this.lblgecensure.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // lblhamlesayisi
+            // 
+            this.lblhamlesayisi.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblhamlesayisi.ForeColor = System.Drawing.Color.White;
+            this.lblhamlesayisi.Location = new System.Drawing.Point(171, 104);
+            this.lblhamlesayisi.Name = "lblhamlesayisi";
+            this.lblhamlesayisi.Size = new System.Drawing.Size(276, 20);
+            this.lblhamlesayisi.TabIndex = 13;
+            this.lblhamlesayisi.Text = "0";
+            this.lblhamlesayisi.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.label3.ForeColor = System.Drawing.Color.White;
+            this.label3.Location = new System.Drawing.Point(170, 79);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(277, 20);
+            this.label3.TabIndex = 12;
+            this.label3.Text = "Hamle Sayısı";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // tmrsure
+            // 
+            this.tmrsure.Interval = 1000;
+            this.tmrsure.Tick += new System.EventHandler(this.tmrsure_Tick);
+            // 
+            // panel1
+            // 
+            this.panel1.Controls.Add(this.button4);
+            this.panel1.Controls.Add(this.button5);
+            this.panel1.Controls.Add(this.button2);
+            this.panel1.Controls.Add(this.button1);
+            this.panel1.Controls.Add(this.button7);
+            this.panel1.Controls.Add(this.button6);
+            this.panel1.Controls.Add(this.button9);
+            this.panel1.Controls.Add(this.button8);
+            this.panel1.Enabled = false;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(450, 452);
+            this.panel1.TabIndex = 14;
+            // 
+            // button4
+            // 
+            this.button4.BackgroundImage = global::Puzzle.Properties.Resources.image_part_004;
+            this.button4.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button4.Location = new System.Drawing.Point(0, 150);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(150, 150);
+            this.button4.TabIndex = 3;
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.BackgroundImage = global::Puzzle.Properties.Resources.image_part_005;
+            this.button5.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button5.Location = new System.Drawing.Point(150, 150);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(150, 150);
+            this.button5.TabIndex = 4;
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackgroundImage = global::Puzzle.Properties.Resources.image_part_002;
+            this.button2.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button2.Location = new System.Drawing.Point(150, 0);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(150, 150);
+            this.button2.TabIndex = 1;
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackgroundImage = global::Puzzle.Properties.Resources.image_part_001;
+            this.button1.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button1.Location = new System.Drawing.Point(0, 0);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(150, 150);
+            this.button1.TabIndex = 0;
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button7
+            // 
+            this.button7.BackgroundImage = global::Puzzle.Properties.Resources.image_part_007;
+            this.button7.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button7.Location = new System.Drawing.Point(0, 300);
+            this.button7.Name = "button7";
+            this.button7.Size = new System.Drawing.Size(150, 150);
+            this.button7.TabIndex = 6;
+            this.button7.UseVisualStyleBackColor = true;
+            this.button7.Click += new System.EventHandler(this.button7_Click);
+            // 
+            // button6
+            // 
+            this.button6.BackgroundImage = global::Puzzle.Properties.Resources.image_part_006;
+            this.button6.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button6.Location = new System.Drawing.Point(300, 150);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(150, 150);
+            this.button6.TabIndex = 5;
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // button9
+            // 
+            this.button9.BackgroundImage = global::Puzzle.Properties.Resources.image_part_009;
+            this.button9.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button9.Location = new System.Drawing.Point(300, 300);
+            this.button9.Name = "button9";
+            this.button9.Size = new System.Drawing.Size(150, 150);
+            this.button9.TabIndex = 8;
+            this.button9.UseVisualStyleBackColor = true;
+            this.button9.Click += new System.EventHandler(this.button9_Click);
+            // 
+            // button8
+            // 
+            this.button8.BackgroundImage = global::Puzzle.Properties.Resources.image_part_008;
+            this.button8.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
+            this.button8.Location = new System.Drawing.Point(150, 300);
+            this.button8.Name = "button8";
+            this.button8.Size = new System.Drawing.Size(150, 150);
+            this.button8.TabIndex = 7;
+            this.button8.UseVisualStyleBackColor = true;
+            this.button8.Click += new System.EventHandler(this.button8_Click);
+            // 
+            // btnkaristir
+            // 
+            this.btnkaristir.BackColor = System.Drawing.Color.Black;
+            this.btnkaristir.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Zoom;
+            this.btnkaristir.Font = new System.Drawing.Font("Microsoft Sans Serif", 13F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.btnkaristir.ForeColor = System.Drawing.Color.White;
+            this.btnkaristir.Image = global::Puzzle.Properties.Resources.karisik2;
+            this.btnkaristir.ImageAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.btnkaristir.Location = new System.Drawing.Point(14, 19);
+            this.btnkaristir.Name = "btnkaristir";
+            this.btnkaristir.Size = new System.Drawing.Size(150, 90);
+            this.btnkaristir.TabIndex = 0;
+            this.btnkaristir.Text = "Karıştır";
+            this.btnkaristir.TextAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.btnkaristir.TextImageRelation = System.Windows.Forms.TextImageRelation.ImageAboveText;
+            this.btnkaristir.UseVisualStyleBackColor = false;
+            this.btnkaristir.Click += new System.EventHandler(this.btnkaristir_Click);
+            // 
+            // label2
+            // 
+            this.label2.BackColor = System.Drawing.Color.White;
+            this.label2.ForeColor = System.Drawing.SystemColors.ControlLightLight;
+            this.label2.Location = new System.Drawing.Point(171, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(276, 10);
+            this.label2.TabIndex = 15;
+            this.label2.Text = "label2";
+            // 
+            // panel2
+            // 
+            this.panel2.Controls.Add(this.btnkaristir);
+            this.panel2.Controls.Add(this.label2);
+            this.panel2.Controls.Add(this.label1);
+            this.panel2.Controls.Add(this.lblgecensure);
+            this.panel2.Controls.Add(this.lblhamlesayisi);
+            this.panel2.Controls.Add(this.label3);
+            this.panel2.Location = new System.Drawing.Point(0, 460);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(450, 133);
+            this.panel2.TabIndex = 16;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Maroon;
+            this.ClientSize = new System.Drawing.Size(450, 607);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.Fixed3D;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.MaximumSize = new System.Drawing.Size(470, 650);
+            this.MinimumSize = new System.Drawing.Size(470, 650);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Puzzle";
+            this.panel1.ResumeLayout(false);
+            this.panel2.ResumeLayout(false);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button9;
+        private System.Windows.Forms.Button button8;
+        private System.Windows.Forms.Button button7;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button btnkaristir;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblgecensure;
+        private System.Windows.Forms.Label lblhamlesayisi;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Timer tmrsure;
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Panel panel2;
+    }
+}
+
---
+++ Puzzle/Form1.cs
@@ -0,0 +1,178 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Puzzle
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+
+
+        int hamle;
+        Random rnd = new Random();
+        Point konum = new Point();
+        Point gecicikonum = new Point();
+        Point[] konumlar = new Point[8];
+        public void OyunBaslat()
+        {
+            hamle = 0;
+            konum = new Point(300, 0);
+            gecicikonum = new Point(0, 0);
+            konumlar = new Point[8] { new Point(0, 0), new Point(150, 0), new Point(0, 150), new Point(150, 150), new Point(300, 150), new Point(0, 300), new Point(150, 300), new Point(300, 300) };
+        }
+
+        public void Kontrol()
+        {
+            if (button1.Location==new Point(0,0) && button2.Location==new Point(150,0) &&  button4.Location==new Point(0,150)
+               && button5.Location==new Point(150,150) && button6.Location==new Point(300,150) && button7.Location==new Point(0,300)
+               && button8.Location==new Point(150,300) && button9.Location==new Point(300,300) )
+            {
+                MessageBox.Show("Tebrikler, Puzzle parçalarını düzgün bir şekilde yerlerine yerleştirdiniz!", "Bildirim", MessageBoxButtons.OK, MessageBoxIcon.Information);
+                lblhamlesayisi.Text = hamle + " hamlede tamamlandı";
+                lblgecensure.Text = hour + ":" + minute + ":" + second + " sürede tamamlandı";
+                tmrsure.Enabled = false;
+                panel1.Enabled = false;
+            }
+        }
+
+        public void kisaltilmiskod()
+        {
+            konum = gecicikonum;
+            hamle++;
+            lblhamlesayisi.Text = hamle + ". hamle";
+            Kontrol();
+        }
+
+        public void Zaman()
+        {
+            if (Convert.ToString(saat).Length == 1) hour = "0" + saat.ToString();
+            else hour = saat.ToString();
+            if (Convert.ToString(dakika).Length == 1) minute = "0" + dakika.ToString();
+            else minute = dakika.ToString();
+            if (Convert.ToString(saniye).Length == 1) second = "0" + saniye.ToString();
+            else second = saniye.ToString();
+            lblgecensure.Text = hour + ":" + minute + ":" + second;
+        }
+
+
+
+        private void btnkaristir_Click(object sender, EventArgs e)
+        {
+            OyunBaslat();
+            saniye=0;
+            dakika = 0;
+            saat = 0;
+            hamle = 0;
+            panel1.Enabled = true;
+            tmrsure.Enabled = true;
+            lblhamlesayisi.Text = hamle.ToString();
+            Zaman();
+           
+
+            for (int i = 0; i < 8; i++)
+            {
+                int index = rnd.Next(8);
+                Point temp = konumlar[index];
+                konumlar[index] = konumlar[i];
+                konumlar[i] = temp;
+            }
+            button1.Location = konumlar[0];
+            button2.Location = konumlar[1];
+            button4.Location = konumlar[2];
+            button5.Location = konumlar[3];
+            button6.Location = konumlar[4];
+            button7.Location = konumlar[5];
+            button8.Location = konumlar[6];
+            button9.Location = konumlar[7];
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            gecicikonum = button1.Location;
+            button1.Location = konum;
+            kisaltilmiskod();
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            gecicikonum = button2.Location;
+            button2.Location = konum;
+            kisaltilmiskod();
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            gecicikonum = button4.Location;
+            button4.Location = konum;
+            kisaltilmiskod();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            gecicikonum = button5.Location;
+            button5.Location = konum;
+            kisaltilmiskod();
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            gecicikonum = button6.Location;
+            button6.Location = konum;
+            kisaltilmiskod();
+        }
+
+        private void button7_Click(object sender, EventArgs e)
+        {
+            gecicikonum = button7.Location;
+            button7.Location = konum;
+            kisaltilmiskod();
+        }
+
+        private void button8_Click(object sender, EventArgs e)
+        {
+            gecicikonum = button8.Location;
+            button8.Location = konum;
+            kisaltilmiskod();
+        }
+
+        private void button9_Click(object sender, EventArgs e)
+        {
+            gecicikonum = button9.Location;
+            button9.Location = konum;
+            kisaltilmiskod();
+        }
+
+
+        int saniye = 0, dakika = 0, saat = 0;
+        string second, minute, hour;
+
+        private void tmrsure_Tick(object sender, EventArgs e)
+        {
+            saniye++;
+            if (saniye == 60)
+            {
+                saniye = 0;
+                dakika++;
+            }
+            if (dakika == 60)
+            {
+                saniye = 0;
+                dakika = 0;
+                saat++;
+            }
+            Zaman();
+        }
+    }
+}
